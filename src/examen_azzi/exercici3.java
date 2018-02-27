b95186e30b838e60a8c69616608d0eaea8d63a57
Usuario <Usuario@192.168.1.35>
2018-02-27 09:40:06 +0100

    Pujada

---
+++ src/examen_azzi/exercici3.java
@@ -0,0 +1,23 @@
+package examen_azzi;
+
+
+
+public class exercici3 {
+
+	public static void main (String[] args){
+		
+		printOne();
+		printOne();
+		printOne();
+		
+	}
+	
+	public static void printOne(){
+		System.out.println("Hello World");
+	}
+	
+	public static void printTo(){
+		printOne();
+		printOne();
+	}
+}
